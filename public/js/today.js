92bc49d359a19c17a78423bb875a637fdc856fd0
Tim Padjen <tpadjen@gmail.com>
2017-02-03 09:35:39 -0700

    Use American date format

--- public/js/today.js
+++ public/js/today.js
@@ -1,11 +1,2 @@
 var today = new Date();
-
-var dd = today.getDate();
-var mm = today.getMonth()+1;
-var yyyy = today.getFullYear();
-
-// if(mm<10){
-//     mm='0'+mm;
-// }
-
-document.getElementById("date").textContent = dd+'/'+mm+'/'+yyyy;
+document.getElementById("date").textContent = (today.getMonth()+1) +'/' + today.getDate() + '/' + today.getFullYear();
